7ee6c41cf0a47ac795065f13b8a075e78c5fc5bc
Jasmine Rivera <jasmineannrivera@gmail.com>
2021-02-28 21:03:25 -0600

    more elements and styling to lg nav

--- js/index.js
+++ js/index.js
@@ -7,6 +7,16 @@ $("#show-mobile-footer").click(function() {
     $("#expanded-text").toggleClass("d-none");
 });
 
+$("#yellow").hover(
+    function() {
+        $(this).css("background-color", "#E0A526");
+    },
+    function() {
+        $(this).css("background-color", "#FEDB01")
+
+    }
+);
+
 
 
 
